d7dd5cb976fd476e810500519ee51aa3233066cd
yash patle <105612209+yashpatle23@users.noreply.github.com>
2025-01-18 00:58:35 +0530

    added video conferenving feature

--- components/navbar.js
+++ components/navbar.js
@@ -42,11 +42,8 @@ const Navbar = ({user}) => {
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                         </svg>
                     </button>
-                     < Link href="/login" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
-                        <svg fill="none" stroke="currentColor" c="round" j="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
-                    </Link>
+                     
+                    
                 </div>
             </header>
 
